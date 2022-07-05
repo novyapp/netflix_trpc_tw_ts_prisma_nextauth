6bc00609dbc40237c247ac303ae8618f1ddccf84
Michał Nowotnik <micnowotnik@gmail.com>
2022-07-05 14:28:15 +0200

    front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({
 }: Props) => {
   //console.log(netflixOriginals);
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <meta name="description" content="Netflix Clone" />
@@ -43,6 +43,7 @@ const Home = ({
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List */}
+
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
