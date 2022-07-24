83439e0bd45e917102cacaf67e6dcc5d0dd8eecf
Michał Nowotnik <micnowotnik@gmail.com>
2022-07-24 12:28:54 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,9 +59,6 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24">
-          {mymovies?.length > 0 ? (
-            <Row title="My Movies" movies={mymovies} />
-          ) : null}
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
